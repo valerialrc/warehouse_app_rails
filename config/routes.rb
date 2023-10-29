740e54a314172069621a99b2b741ddc8fdc668a1
Valéria Carneiro <valerialribeiroc@gmail.com>
2023-10-29 20:03:17 -0300

    Usuário vê detalhes do galpão e volta para a tela inicial

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get '/', to: 'home#index'
+  root to: 'home#index'
+  resources :warehouses, only: [:show]
 end
--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -5,7 +5,7 @@ describe 'Usuário visita tela inicial' do
     # Arrange
 
     # Act
-    visit('/')
+    visit root_path
 
     # Assert
     expect(page).to have_content('Galpões & Estoque')
@@ -17,7 +17,7 @@ describe 'Usuário visita tela inicial' do
     Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)    
     Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000)    
     # Act
-    visit('/')
+    visit root_path
 
     # Assert
     expect(page).not_to have_content('Não existem galpões cadastrados')
@@ -36,7 +36,7 @@ describe 'Usuário visita tela inicial' do
     # Arrange
 
     # Act
-    visit('/')
+    visit root_path
     # Assert
     expect(page).to have_content('Não existem galpões cadastrados')
   end
